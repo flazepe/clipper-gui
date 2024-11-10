9e16047172135cf74c69568b28b35075d8a7a8b7
flazepe <51487578+flazepe@users.noreply.github.com>
2024-11-10 15:38:03 +0800

    fix: listeners and attributes

--- src/components/SideInput.tsx
+++ src/components/SideInput.tsx
@@ -28,12 +28,10 @@ export default function ({ input }: { input: Input }) {
 	return (
 		<div
 			ref={setDroppableNodeRef}
-			{...listeners}
-			{...attributes}
 			style={{ transform: CSS.Translate.toString(transform), opacity: transform ? 0.7 : 1 }}
 			className="flex cursor-pointer flex-col text-lg"
 		>
-			<div ref={setNodeRef} className="cursor-move rounded-t bg-gray-800 p-2 text-center">
+			<div ref={setNodeRef} {...listeners} {...attributes} className="cursor-move rounded-t bg-gray-800 p-2 text-center">
 				{input.file.split(/[/\\]/).pop()}
 			</div>
 			<div className="flex">
