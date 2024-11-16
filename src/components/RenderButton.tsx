5569ae155750c5cde5977950fe7460731a654509
flazepe <51487578+flazepe@users.noreply.github.com>
2024-11-17 01:31:38 +0800

    refactor: use existing function instead of `exists()` * i'm so dumb, why didn't i use something i already had???

--- src/components/RenderButton.tsx
+++ src/components/RenderButton.tsx
@@ -1,10 +1,10 @@
 import { ButtonComponent } from "@/components";
-import { getFfmpegArgs, Render, SUPPORTED_EXTENSIONS } from "@/functions/clipper";
+import { getFfmpegArgs, isValidVideo, Render, SUPPORTED_EXTENSIONS } from "@/functions/clipper";
 import { durationToSeconds, secondsToDuration } from "@/functions/seconds";
 import { VideoVintageIcon } from "@/icons";
 import { writeText } from "@tauri-apps/plugin-clipboard-manager";
 import { message, save } from "@tauri-apps/plugin-dialog";
-import { exists, writeTextFile } from "@tauri-apps/plugin-fs";
+import { writeTextFile } from "@tauri-apps/plugin-fs";
 import { Command } from "@tauri-apps/plugin-shell";
 import { useContext } from "react";
 import { EncoderStateContext, InputsStateContext, OutputContext, RendersStateContext } from "../contexts";
@@ -27,7 +27,7 @@ export default function () {
 					if (!input.segments[0]) return message(`Input "${input.file}" is missing segments.`, { kind: "error" });
 
 					// Deleted inputs
-					if (!(await exists(input.file).catch(() => true))) return message(`Input "${input.file}" is deleted.`, { kind: "error" });
+					if (!(await isValidVideo(input.file))) return message(`Input "${input.file}" is deleted.`, { kind: "error" });
 				}
 
 				const split = inputs.inputs[0].file.split("."),
