fec21e311aecef9bac5fcc17a43a56c40168066f
flazepe <51487578+flazepe@users.noreply.github.com>
2024-11-27 19:08:46 +0800

    feat: disable zoom keys again

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
+// @ts-expect-error `process` is a Node.js global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
@@ -9,27 +9,18 @@ export default defineConfig({
 	resolve: {
 		alias: [{ find: "@", replacement: "/src" }]
 	},
-
 	plugins: [react()],
-
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	//
-	// 1. prevent vite from obscuring rust errors
+	// 1. Prevent Vite from obscuring Rust errors
 	clearScreen: false,
-	// 2. tauri expects a fixed port, fail if that port is not available
+	// 2. Tauri expects a fixed port, fail if that port is not available
 	server: {
 		port: 1420,
 		strictPort: true,
 		host: host || false,
-		hmr: host
-			? {
-					protocol: "ws",
-					host,
-					port: 1421
-				}
-			: undefined,
+		hmr: host ? { protocol: "ws", host, port: 1421 } : undefined,
 		watch: {
-			// 3. tell vite to ignore watching `src-tauri`
+			// 3. tell Vite to ignore watching `src-tauri`
 			ignored: ["**/src-tauri/**"]
 		}
 	}
