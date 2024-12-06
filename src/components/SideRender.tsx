49b67770bfa6ef9b11f4dd1a41c240ab4ec03d02
flazepe <51487578+flazepe@users.noreply.github.com>
2024-12-06 14:50:10 +0800

    fix: in case close event is not emitted

--- src/components/SideRender.tsx
+++ src/components/SideRender.tsx
@@ -1,7 +1,13 @@
 import { Render } from "@/functions/clipper";
 import { CancelIcon } from "@/icons";
+import StatesContext from "@/StatesContext";
+import { useContext } from "react";
 
 export default function ({ render }: { render: Render }) {
+	const {
+		renders: [, setRenders]
+	} = useContext(StatesContext);
+
 	return (
 		<div className="flex flex-col text-lg">
 			<div className="rounded-t bg-gray-800 p-2 text-center">{render.filename.split(/[/\\]/).pop()}</div>
@@ -9,7 +15,10 @@ export default function ({ render }: { render: Render }) {
 				<div className="h-full bg-green-500" style={{ width: `${render.progress}%` }}></div>
 			</div>
 			<div
-				onClick={() => render.child.kill().catch(() => null)}
+				onClick={() => {
+					render.child.kill().catch(() => null);
+					setRenders?.(renders => renders.filter(entry => entry !== render)); // In case killing does not emit the close event
+				}}
 				className="flex cursor-pointer items-center justify-center gap-2 rounded-b bg-red-600 p-2 uppercase"
 			>
 				<CancelIcon className="w-7 fill-white" />
